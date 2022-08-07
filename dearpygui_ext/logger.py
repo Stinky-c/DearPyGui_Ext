a482355d37c3c209af4bbeaa15bee51c6d9162f7
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2022-08-07 13:33:40 -0700

    new formatting now uses match casing

--- dearpygui_ext/logger.py
+++ dearpygui_ext/logger.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 import dearpygui.dearpygui as dpg
 
 class mvLogger:
 
     def __init__(self, parent=None):
-        
+
         self.log_level = 0
         self._auto_scroll = True
         self.filter_id = None
@@ -23,29 +24,26 @@ class mvLogger:
         self.child_id = dpg.add_child_window(parent=self.window_id, autosize_x=True, autosize_y=True)
         self.filter_id = dpg.add_filter_set(parent=self.child_id)
 
-        with dpg.theme() as self.trace_theme:
-            with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (0, 255, 0, 255))
 
         with dpg.theme() as self.debug_theme:
             with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (64, 128, 255, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (64, 128, 255, 255)) # blue
 
         with dpg.theme() as self.info_theme:
             with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 255, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 255, 255)) # white
 
         with dpg.theme() as self.warning_theme:
             with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 0, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 0, 255)) # yellow
 
         with dpg.theme() as self.error_theme:
             with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255)) # red
 
         with dpg.theme() as self.critical_theme:
             with dpg.theme_component(0):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255)) # red
 
     def auto_scroll(self, value):
         self._auto_scroll = value
@@ -62,23 +60,23 @@ class mvLogger:
 
         theme = self.info_theme
 
-        if level == 0:
-            message = "[TRACE]\t\t" + message
-            theme = self.trace_theme
-        elif level == 1:
-            message = "[DEBUG]\t\t" + message
-            theme = self.debug_theme
-        elif level == 2:
-            message = "[INFO]\t\t" + message
-        elif level == 3:
-            message = "[WARNING]\t\t" + message
-            theme = self.warning_theme
-        elif level == 4:
-            message = "[ERROR]\t\t" + message
-            theme = self.error_theme
-        elif level == 5:
-            message = "[CRITICAL]\t\t" + message
-            theme = self.critical_theme
+        # removed trace and update formatting
+        match level:
+            case 1:
+                message = f"[{datetime.now().strftime('%H:%M:%S')}] [DEBUG]    {message}"
+                theme = self.debug_theme
+            case 2:
+                message = f"[{datetime.now().strftime('%H:%M:%S')}] [INFO]    {message}"
+                theme = self.info_theme
+            case 3:
+                message = f"[{datetime.now().strftime('%H:%M:%S')}] [WARNING]    {message}"
+                theme = self.warning_theme
+            case 4:
+                message = f"[{datetime.now().strftime('%H:%M:%S')}] [ERROR]    {message}"
+                theme = self.error_theme
+            case 5:
+                message = f"[{datetime.now().strftime('%H:%M:%S')}] [CRITICAL]    {message}"
+                theme = self.critical_theme
 
         new_log = dpg.add_text(message, parent=self.filter_id, filter_key=message)
         dpg.bind_item_theme(new_log, theme)
@@ -86,22 +84,20 @@ class mvLogger:
             scroll_max = dpg.get_y_scroll_max(self.child_id)
             dpg.set_y_scroll(self.child_id, -1.0)
 
-    def log(self, message):
-        self._log(message, 0)
 
-    def log_debug(self, message):
+    def debug(self, message):
         self._log(message, 1)
 
-    def log_info(self, message):
+    def info(self, message):
         self._log(message, 2)
 
-    def log_warning(self, message):
+    def warning(self, message):
         self._log(message, 3)
 
-    def log_error(self, message):
+    def error(self, message):
         self._log(message, 4)
 
-    def log_critical(self, message):
+    def critical(self, message):
         self._log(message, 5)
 
     def clear_log(self):
